8310fc34976a691b3f36befd0a0799c019535bc4
fendoudebb <15317832520@163.com>
2020-12-02 15:59:25 +0800

    添加 /admin/

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/admin/',
     proxyTable: {
       '/api': {
         target: 'http://localhost:8080', // 接口的域名
@@ -52,7 +52,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/admin/',
 
     /**
      * Source Maps
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 // import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routes from './routers'
-import store from '@/store'
 import iView from 'iview'
 import { canTurnTo } from '@/libs/util'
 
 // Vue.use(Router);
 const router = new VueRouter({
   mode: 'history',
+  base: '/admin',
   routes
 });
 
