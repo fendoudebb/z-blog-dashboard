e67476c29951d812d17d6801542f67afcfaced42
fendoudebb <15317832520@163.com>
2021-04-03 10:10:11 +0800

    点击图标跳转首页

--- src/components/main/Main.vue
+++ src/components/main/Main.vue
@@ -3,7 +3,7 @@
     <Sider hide-trigger collapsible :width="210" :collapsed-width="64" v-model="collapsed">
       <side-menu accordion :active-name="$route.name" :collapsed="collapsed" @on-select="turnToPage" :menu-list="menuList">
         <!-- 需要放在菜单上面的内容，如Logo，写在side-menu标签内部，如下 -->
-        <div class="logo-con">
+        <div class="logo-con" @click="jumpToHome">
           <img v-show="!collapsed" :src="maxLogo" key="max-logo" />
           <img v-show="collapsed" :src="minLogo" key="min-logo" />
         </div>
@@ -80,6 +80,11 @@ export default {
     },
     handleClick (item) {
       this.turnToPage(item.name)
+    },
+    jumpToHome() {
+      this.$router.push({
+        name: '首页'
+      })
     }
   },
   watch: {
